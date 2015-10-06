2afc55b07628a808ea7fd3ac2b82c94937fcc786
Mathusha Senthil <mathusha.senthil@gmail.com>
2015-10-06 16:33:53 -0400

    updates

--- thehubproject/db/schema.rb
+++ thehubproject/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006020152) do
+ActiveRecord::Schema.define(version: 20151006202414) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20151006020152) do
     t.string   "salt"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
