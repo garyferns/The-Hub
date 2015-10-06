728ba5d54765f10d80d3e989ee16c0e5bbcc9b6a
Mathusha Senthil <mathusha.senthil@gmail.com>
2015-10-06 16:19:49 -0400

    Added Cacoon, Sorcery. Implemented user logins

--- thehubproject/db/migrate/20151006020152_create_users.rb
+++ thehubproject/db/migrate/20151006020152_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :email
+      t.string :crypted_password
+      t.string :salt
 
       t.timestamps null: false
     end
--- thehubproject/db/schema.rb
+++ thehubproject/db/schema.rb
@@ -14,8 +14,11 @@
 ActiveRecord::Schema.define(version: 20151006020152) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
