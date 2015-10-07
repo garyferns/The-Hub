f504b5b5f50c0da831cd99369801dbaf6ae7c56e
Mathusha Senthil <mathusha.senthil@gmail.com>
2015-10-07 14:35:57 -0400

    Fixed @user bug

--- thehubproject/app/controllers/users_controller.rb
+++ thehubproject/app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
+    if @user.save
       redirect_to root_path
     else
       render :new
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
   end
 
 end
