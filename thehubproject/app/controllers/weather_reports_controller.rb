750098ce68bb396e8cf6665a43dafd33e590528c
Gareth Fernandes <gfernandes_6@hotmail.com>
2015-10-16 13:44:00 -0400

    Added CSS for profile page

--- thehubproject/app/controllers/weather_reports_controller.rb
+++ thehubproject/app/controllers/weather_reports_controller.rb
@@ -1,32 +1,12 @@
 class WeatherReportsController < ApplicationController
 
   def index
-    @weather_report = WeatherReport.new('Cairo')
+    @weather_report = WeatherReport.new("Toronto")
     @weather_report.get_report
   end
-
-  # def new
-  #   @weatherman = WeatherReport.new
-  # end
-
-  # def show
-  #   @weather_report = WeatherReport.find(params[:id])
-  #   @comment = @weather_report.comments.build
-  # end
-
-  # def create
-  #   @weather_report = WeatherReport.new(weather_report_params)
-
-  #   if @weatherman.save
-  #     redirect_to weather_reports_url
-  #   else
-  #     render :new
-  #   end
-  # end
 end
 
-#   private
-#   def weather_report_params
-#     params.require(:post).permit(:title, :body, :author)
-#   end
-# end
+  # private
+  # def weather_report_params
+  #   params.require(:post).permit(:city)
+  # end
--- thehubproject/app/models/weather_report.rb
+++ thehubproject/app/models/weather_report.rb
@@ -6,6 +6,7 @@ class WeatherReport
   include HTTParty
   base_uri 'api.openweathermap.org/data/2.5'
 
+
   def initialize(city)
     @city = city
   end
